e4c85215be9e1308fc79f29296e5aa19c810d193
권나영 <dek3127@gmail.com>
2022-11-06 15:55:39 +0900

    221106_백준_16472
    - 1차 실패한 16472

---
+++ 11월1주차/naa0_16472_1차(실패).py
@@ -0,0 +1,43 @@
+# 입력 테스트용
+import os
+currentPath = os.getcwd()
+os.chdir(currentPath)
+print(currentPath)
+import sys
+sys.stdin = open(currentPath + "/11월1주차/백준_16472.txt", "r")
+
+# 입력 값 받기
+n = int(input())
+data = input()
+# 끝 포인터
+end = 1
+# 문자열 길이
+N = len(data)
+# 정답
+ans = 0
+
+from collections import deque
+
+# 각 알파벳 마지막 인덱스
+alphabet_dict = dict()
+
+for start in range(N):
+    alphabet_dict.setdefault(data[start], 0)
+    alphabet_dict[data[start]] += 1
+
+    end = start + 1
+    while len(alphabet_dict) <= n and end < N:
+        if data[end] in alphabet_dict.keys():
+            alphabet_dict[data[end]] += 1
+        else:
+            alphabet_dict.setdefault(data[end], 0)
+        end += 1
+    alphabet_dict[data[start]] -= 1
+
+    if not alphabet_dict[data[start]]:
+        del alphabet_dict[data[start]]
+    # print(alphabet_dict)
+    # print(start, end)
+    ans = max(ans, end - start)
+print(ans)
+        
